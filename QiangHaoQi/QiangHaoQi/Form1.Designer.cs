0439b98fd4e16deb0bdf1c05e8ca70fe341716da
whshiyun <2999049@qq.com>
2014-11-27 14:36:58 +0800

    creat
    creat 2014-11-27

---
+++ QiangHaoQi/QiangHaoQi/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace QiangHaoQi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(366, 238);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(569, 227);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(75, 23);
+            this.login.TabIndex = 1;
+            this.login.Text = "login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(506, 31);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(100, 21);
+            this.username.TabIndex = 2;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(506, 88);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(100, 21);
+            this.password.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(432, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "username : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(432, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "password : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ QiangHaoQi/QiangHaoQi/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace QiangHaoQi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            crifanLib crifan = new crifanLib();
+
+            //textBox1.Text = "aaaaaaaaaaaaa";
+            string url = "http://jsonp.yihu.com/action/login/login.ashx?d=1416883229934&loginid=18120558799&pwd=112200&type=1&CkSavePwd=n&callback=_jqjsp&_1416883229934=";
+            CookieContainer cookieContainer = new CookieContainer();
+            //cookieContainer.Add()
+            //用url实例化HttpWebRequest类
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+
+            //填充数据包的信息，填充的值都是在数据包查找对应的信息得到的
+            request.CookieContainer = cookieContainer;
+            request.Referer = url;
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Accept = "*/*";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36";
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //foreach (Cookie cookie in response.Cookies)
+            //{
+            //    cookieContainer.Add(cookie);
+            //}
+            cookieContainer.Add(response.Cookies);
+            //crifan.addCookieToCookies(response.Cookies);
+
+            Stream stream = response.GetResponseStream();  //转换为数据流
+            StreamReader reader = new StreamReader(stream);
+            string html = reader.ReadToEnd();   //通过StreamReader类读取流
+            //reader.Close();
+            //stream.Close();
+            textBox1.Text = html;
+
+
+            string url1 = "http://www.yihu.com/";//"http://www.yihu.com/#";
+            string url2 = "http://sr2.yihuimg.com/js/default.js?yj=2014/11/27%2010:46:45";
+            string url3 = "http://hm.baidu.com/hm.gif?cc=1&ck=1&cl=24-bit&ds=1680x1050&ep=12856%2C4994&et=3&fl=15.0&ja=1&ln=zh-CN&lo=0&lt=1416881715&nv=0&rnd=376184298&si=50a96b999b752ef15792867dfda15c2a&st=4&su=http%3A%2F%2Fwww.yihu.co";
+
+            request = (HttpWebRequest)HttpWebRequest.Create(url1);
+            //HttpWebRequest request1 = (HttpWebRequest)HttpWebRequest.Create(url);
+            request.CookieContainer = cookieContainer;
+            request.Referer = url;
+            //request.ContentType = "application/x-www-form-urlencoded";
+            request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36";
+            request.Method = "GET";
+            
+            response = (HttpWebResponse)request.GetResponse();
+if (false)
+{
+                request = (HttpWebRequest)HttpWebRequest.Create(url2);
+                //HttpWebRequest request1 = (HttpWebRequest)HttpWebRequest.Create(url);
+                request.CookieContainer = cookieContainer;
+                request.Referer = url;
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36";
+                request.Method = "GET";
+                response = (HttpWebResponse)request.GetResponse();
+
+                request = (HttpWebRequest)HttpWebRequest.Create(url3);
+                //HttpWebRequest request1 = (HttpWebRequest)HttpWebRequest.Create(url);
+                request.CookieContainer = cookieContainer;
+                request.Referer = url;
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36";
+                request.Method = "GET";
+                response = (HttpWebResponse)request.GetResponse();
+}
+            string htmlCharset = "UTF-8";
+            //use songtaste's html's charset GB2312 to decode html
+            //otherwise will return messy code
+            Encoding htmlEncoding = Encoding.GetEncoding(htmlCharset);
+            //StreamReader sr = new StreamReader(response.GetResponseStream(), htmlEncoding);
+            reader = new StreamReader(response.GetResponseStream(), htmlEncoding);
+            string respHtml = reader.ReadToEnd();
+            textBox1.Text = respHtml;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
