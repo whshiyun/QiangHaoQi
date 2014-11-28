fc946b1098c988ae4214e138e2b15ad3d2587257
whshiyun <2999049@qq.com>
2014-11-28 16:00:43 +0800

    2014-11-28
    v1

--- QiangHaoQi/QiangHaoQi/Form1.Designer.cs
+++ QiangHaoQi/QiangHaoQi/Form1.Designer.cs
@@ -34,7 +34,15 @@
             this.password = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Departments = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DepartmentOk = new System.Windows.Forms.Button();
+            this.DocOk = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Doctor = new System.Windows.Forms.ComboBox();
+            this.TimeOk = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -44,11 +52,10 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(366, 399);
             this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // login
             // 
-            this.login.Location = new System.Drawing.Point(574, 12);
+            this.login.Location = new System.Drawing.Point(661, 40);
             this.login.Name = "login";
             this.login.Size = new System.Drawing.Size(75, 23);
             this.login.TabIndex = 1;
@@ -58,52 +65,131 @@
             // 
             // username
             // 
-            this.username.Location = new System.Drawing.Point(468, 12);
+            this.username.Location = new System.Drawing.Point(455, 12);
             this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(100, 21);
+            this.username.Size = new System.Drawing.Size(200, 21);
             this.username.TabIndex = 2;
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(468, 39);
+            this.password.Location = new System.Drawing.Point(455, 42);
             this.password.Name = "password";
             this.password.PasswordChar = '*';
-            this.password.Size = new System.Drawing.Size(100, 21);
+            this.password.Size = new System.Drawing.Size(200, 21);
             this.password.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(394, 16);
+            this.label1.Location = new System.Drawing.Point(384, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(71, 12);
             this.label1.TabIndex = 4;
             this.label1.Text = "username : ";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(394, 43);
+            this.label2.Location = new System.Drawing.Point(384, 45);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 12);
             this.label2.TabIndex = 5;
             this.label2.Text = "password : ";
             // 
-            // comboBox1
+            // Departments
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(468, 98);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 20);
-            this.comboBox1.TabIndex = 6;
+            this.Departments.FormattingEnabled = true;
+            this.Departments.Location = new System.Drawing.Point(455, 78);
+            this.Departments.Name = "Departments";
+            this.Departments.Size = new System.Drawing.Size(200, 20);
+            this.Departments.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(384, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "选择科室：";
+            // 
+            // DepartmentOk
+            // 
+            this.DepartmentOk.Location = new System.Drawing.Point(661, 78);
+            this.DepartmentOk.Name = "DepartmentOk";
+            this.DepartmentOk.Size = new System.Drawing.Size(75, 23);
+            this.DepartmentOk.TabIndex = 8;
+            this.DepartmentOk.Text = "Ok";
+            this.DepartmentOk.UseVisualStyleBackColor = true;
+            this.DepartmentOk.Click += new System.EventHandler(this.DepartmentOk_Click);
+            // 
+            // DocOk
+            // 
+            this.DocOk.Location = new System.Drawing.Point(661, 112);
+            this.DocOk.Name = "DocOk";
+            this.DocOk.Size = new System.Drawing.Size(75, 23);
+            this.DocOk.TabIndex = 11;
+            this.DocOk.Text = "Ok";
+            this.DocOk.UseVisualStyleBackColor = true;
+            this.DocOk.Click += new System.EventHandler(this.DocOk_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(384, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "选择医生：";
+            // 
+            // Doctor
+            // 
+            this.Doctor.FormattingEnabled = true;
+            this.Doctor.Location = new System.Drawing.Point(455, 112);
+            this.Doctor.Name = "Doctor";
+            this.Doctor.Size = new System.Drawing.Size(200, 20);
+            this.Doctor.TabIndex = 9;
+            // 
+            // TimeOk
+            // 
+            this.TimeOk.Location = new System.Drawing.Point(661, 147);
+            this.TimeOk.Name = "TimeOk";
+            this.TimeOk.Size = new System.Drawing.Size(75, 23);
+            this.TimeOk.TabIndex = 14;
+            this.TimeOk.Text = "Ok";
+            this.TimeOk.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(384, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "选择时间：";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(455, 147);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(200, 20);
+            this.comboBox3.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(656, 423);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(748, 423);
+            this.Controls.Add(this.TimeOk);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.DocOk);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Doctor);
+            this.Controls.Add(this.DepartmentOk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Departments);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.password);
@@ -125,7 +211,15 @@
         private System.Windows.Forms.TextBox password;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox Departments;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button DepartmentOk;
+        private System.Windows.Forms.Button DocOk;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox Doctor;
+        private System.Windows.Forms.Button TimeOk;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
 
--- QiangHaoQi/QiangHaoQi/Form1.cs
+++ QiangHaoQi/QiangHaoQi/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using System.Net;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace QiangHaoQi
 {
@@ -20,9 +21,31 @@ namespace QiangHaoQi
             password.Text = "112200";
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private class NameAndUrl
         {
+            public NameAndUrl(string name, string url)
+            {
+                this.name = name;
+                this.url = url;
+            }
+            public string name;
+            public string url;
+        }
 
+        private const string baseUrl = "http://www.yihu.com/";
+        private List<NameAndUrl> departmentsArr = new List<NameAndUrl>();
+        private List<NameAndUrl> doctorArr = new List<NameAndUrl>();
+        private CookieContainer cookieContainer = new CookieContainer();
+
+        private void Set_NameAndUrl(List<NameAndUrl> list, MatchCollection nameAndUrl)
+        {
+            foreach (Match m in nameAndUrl)
+            {
+                //this.comb_type.Items.Add(new ControlItem(dr,dr["answertypename"].ToString()));
+                //Departments.Items.Add(m.Groups["name"].Value);
+                list.Add(new NameAndUrl(m.Groups["name"].Value, baseUrl + m.Groups["url"].Value));
+            }
+            //Departments.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -30,8 +53,7 @@ namespace QiangHaoQi
             crifanLib crifan = new crifanLib();
 
             //textBox1.Text = "aaaaaaaaaaaaa";
-            string url = "http://jsonp.yihu.com/action/login/login.ashx?d=1416883229934&loginid="+username.Text+"&pwd="+password.Text+"&type=1&CkSavePwd=n&callback=_jqjsp&_1416883229934=";
-            CookieContainer cookieContainer = new CookieContainer();
+            string url = "http://jsonp.yihu.com/action/login/login.ashx?d=1416883229934&loginid=" + username.Text + "&pwd=" + password.Text + "&type=1&CkSavePwd=n&callback=_jqjsp&_1416883229934=";
             //cookieContainer.Add()
             //用url实例化HttpWebRequest类
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
@@ -61,7 +83,8 @@ namespace QiangHaoQi
             textBox1.Text = html;
 
 
-            string url1 = "http://www.yihu.com/Action/Doctor/NewRegCheck.ashx?time=1417097780646&url=http%3A%2F%2Fwww.yihu.com%2Fdoctor%2FNewOrderList.aspx%3Fdoctorsn%3D710036578%26sn%3D121826776%26saleid%3D3565097&membersn=9824864&memberid=1&ID=121826776";
+            string url1 = "http://www.yihu.com/hospital/hb/CE95AEF045804CCE86A04F3191BA6FF2.shtml";
+            //"http://www.yihu.com/Action/Doctor/NewRegCheck.ashx?time=1417097780646&url=http%3A%2F%2Fwww.yihu.com%2Fdoctor%2FNewOrderList.aspx%3Fdoctorsn%3D710036578%26sn%3D121826776%26saleid%3D3565097&membersn=9824864&memberid=1&ID=121826776";
             //"http://www.yihu.com/Action/Doctor/NewMemberBind.ashx?url=http%3A%2F%2Fwww.yihu.com%2Fdoctor%2FNewOrderList.aspx%3Fdoctorsn%3D710036578%26sn%3D121826776%26saleid%3D3565097&d=1417097479373";
             //"http://www.yihu.com/Action/doctor/NumberWater.ashx?sn=710036578&date=2014-12-02&timeid=1&type=3&weekid=2&url=http%253A//www.yihu.com/doctor/ordernumber.aspx%253Fstate%253D4%2526doctorsn%253D710036578%2526registerdate%253D2014-12-02%2526timeid%253D1%2526weekid%253D2%2526ordertype%253D3%2526watersn%253D39428543&state=4&d=1417096199516";
             //"http://www.yihu.com/";
@@ -76,10 +99,10 @@ namespace QiangHaoQi
             request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
             request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36";
             request.Method = "GET";
-            
+
             response = (HttpWebResponse)request.GetResponse();
-if (false)
-{
+            if (false)
+            {
                 request = (HttpWebRequest)HttpWebRequest.Create(url2);
                 //HttpWebRequest request1 = (HttpWebRequest)HttpWebRequest.Create(url);
                 request.CookieContainer = cookieContainer;
@@ -99,7 +122,7 @@ if (false)
                 request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36";
                 request.Method = "GET";
                 response = (HttpWebResponse)request.GetResponse();
-}
+            }
             string htmlCharset = "UTF-8";
             //use songtaste's html's charset GB2312 to decode html
             //otherwise will return messy code
@@ -108,11 +131,108 @@ if (false)
             reader = new StreamReader(response.GetResponseStream(), htmlEncoding);
             string respHtml = reader.ReadToEnd();
             textBox1.Text = respHtml;
+
+
+            //<a href="/dept/0300/0201148ABD1542B89FAEDB94D1C84C59.shtml" title="成人内科">成人内科</a>
+            //string h1userP = @"<a\s+href=""[^""]*""\s+title=""[\u4e00-\u9fa5]*"">(?<h1user>.+?)</a>";//@"<h1\s+class=""h1user"">(?<h1user>.+?)</h1>";
+            //string h1userP = @"<a\s+href="".*""\s+title="".*""\s+class="".*"">(?<h1user>.+?)</a>";
+            //string h1userP = @"<a\s+href=""[^""]*""\s+title=""[^""]*"">(?<name>.+?)</a>";
+
+            string h1userP = @"<a\s+href=""(?<url>[^""]+?)""\s+title=""[^""]*"">(?<name>.+?)</a>";
+            //Match foundH1user = (new Regex(h1userP)).Match(textBox1.Text);
+            MatchCollection foundH1user = (new Regex(h1userP)).Matches(textBox1.Text);
+            //MatchCollection foundH1user = Regex.Matches(textBox1.Text, h1userP);
+            //Match foundH1user = Regex.Match(textBox1.Text, h1userP);
+            //textBox1.Text = "";
+            foreach (Match m in foundH1user)
+            {
+                if (m.Success)
+                {
+                    //extracted the expected h1user's value
+                    //textBox1.Text += m.Groups["name"].Value;
+                   // textBox1.Text += m.Groups["url"].Value;
+                    //textBox1.Text += "\r\n";
+                    Departments.Items.Add(m.Groups["name"].Value);
+                }
+                else
+                {
+                    textBox1.Text += "Not found h1 user !";
+                }
+            }
+            Departments.SelectedIndex = 0 ;
+
+            Set_NameAndUrl(departmentsArr, foundH1user);
+
+            reader.Close();
+            stream.Close();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void DepartmentOk_Click(object sender, EventArgs e)
         {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(departmentsArr[Departments.SelectedIndex].url);
 
+            //填充数据包的信息，填充的值都是在数据包查找对应的信息得到的
+            request.CookieContainer = cookieContainer;
+            request.Referer = departmentsArr[Departments.SelectedIndex].url;
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Accept = "*/*";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36";
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream stream = response.GetResponseStream();  //转换为数据流
+            StreamReader reader = new StreamReader(stream);
+            string html = reader.ReadToEnd();   //通过StreamReader类读取流
+            reader.Close();
+            stream.Close();
+            textBox1.Text = html;
+
+            //<a target="_blank" href="/doctor/hb/EE8DC590BDB14B069C5F1D95FAC060DD.shtml?sn=22573" title="黄玉兰">黄玉兰</a>
+            //string h1userP = @"<a\s+href=""(?<url>[^""]+?)""\s+title=""[^""]*"">(?<name>.+?)</a>";
+            string h1userP = @"<a\s+target=""[^""]*""\s+href=""(?<url>[^""]+?)""\s+title=""[^""]*"">(?<name>.+?)</a>";
+            MatchCollection foundH1user = (new Regex(h1userP)).Matches(textBox1.Text);
+            textBox1.Text = "";
+            foreach (Match m in foundH1user)
+            {
+                if (m.Success)
+                {
+                    //extracted the expected h1user's value
+                    textBox1.Text += m.Groups["name"].Value;
+                    textBox1.Text += m.Groups["url"].Value;
+                    textBox1.Text += "\r\n";
+                    Doctor.Items.Add(m.Groups["name"].Value);
+                }
+                else
+                {
+                    textBox1.Text += "Not found h1 user !";
+                }
+            }
+            Doctor.SelectedIndex = 0;
+
+            Set_NameAndUrl(doctorArr, foundH1user);
+        }
+
+        private void DocOk_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(doctorArr[Doctor.SelectedIndex].url);
+
+            //填充数据包的信息，填充的值都是在数据包查找对应的信息得到的
+            request.CookieContainer = cookieContainer;
+            request.Referer = doctorArr[Doctor.SelectedIndex].url;
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Accept = "*/*";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36";
+            request.Method = "GET";
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream stream = response.GetResponseStream();  //转换为数据流
+            StreamReader reader = new StreamReader(stream);
+            string html = reader.ReadToEnd();   //通过StreamReader类读取流
+            reader.Close();
+            stream.Close();
+            textBox1.Text = html;
         }
     }
 }
